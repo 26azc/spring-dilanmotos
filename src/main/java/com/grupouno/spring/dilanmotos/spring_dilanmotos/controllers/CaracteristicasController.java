aa48e90467ba116bb7cedbf6370d71fd6e317672
Andres Cruz <bjuan8528gmail.com>
2025-11-12 19:38:25 -0500

    MySQL

---
+++ src/main/java/com/grupouno/spring/dilanmotos/spring_dilanmotos/controllers/CaracteristicasController.java
@@ -0,0 +1,33 @@
+package com.grupouno.spring.dilanmotos.spring_dilanmotos.controllers;
+
+
+public class CaracteristicasController {
+
+}
+/*
+ * 
+ * package com.grupouno.spring.dilanmotos.spring_dilanmotos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.grupouno.spring.dilanmotos.spring_dilanmotos.models.Usuarios;
+import org.springframework.ui.Model;
+import java.util.List;
+import java.util.Arrays; // añadido
+
+@Controller
+public class UsuarioController {
+//http://localhost:8080/usuario
+   @GetMapping("/usuario")
+public String Usuario(Model model){
+    List<Usuarios> lista = Arrays.asList(
+        new Usuarios("Juan Andres", "juan@gmail.com", "flow123", 1),
+        new Usuarios("Ana María", "ana@gmail.com", "clave456", 2)
+    );
+    model.addAttribute("usuarios", lista);
+    return "usuario";
+    }
+}
+
+ */
---
+++ src/main/java/com/grupouno/spring/dilanmotos/spring_dilanmotos/models/Caracteristicas.java
@@ -0,0 +1,40 @@
+package com.grupouno.spring.dilanmotos.spring_dilanmotos.models;
+
+public class Caracteristicas {
+
+    private int idCaracteristica;
+    private int idMoto;
+    private String descripcion;
+
+
+public Caracteristicas(int idCaracteristica, int idMoto, String descripcion){
+
+    this.idCaracteristica = idCaracteristica;
+    this.idMoto = idMoto;
+    this.descripcion = descripcion;
+}
+
+    public int getIdCaracteristica() {
+        return idCaracteristica;
+    }
+
+    public void setIdCaracteristica(int idCaracteristica) {
+        this.idCaracteristica = idCaracteristica;
+    }
+
+    public int getIdMoto() {
+        return idMoto;
+    }
+
+    public void setIdMoto(int idMoto) {
+        this.idMoto = idMoto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
