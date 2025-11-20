ca4a39f4b0329c9600f51a883838df9509a73125
Diver Mauricio Osma Manrique <diver1402@gmail.com>
2025-11-20 11:30:51 -0500

    Actualización, avance vista de moto

--- src/main/java/com/grupouno/spring/dilanmotos/models/Moto.java
+++ src/main/java/com/grupouno/spring/dilanmotos/models/Moto.java
@@ -7,19 +7,25 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.ManyToOne; 
+import jakarta.persistence.JoinColumn; 
 
 @Entity
 @Table(name = "moto")
 public class Moto {
+    
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int idMoto;
     
-    @Column(name = "id_usuario")
-    private int idUsuario;
+    @ManyToOne 
+    @JoinColumn(name = "id_usuario") 
+    private Usuarios usuario; 
     
-    @Column(name = "id_marca")
-    private int idMarca;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_marca") 
+    private Marca marca; 
 
     @NotBlank(message = "El modelo es obligatorio")
     @Column(name = "modelo")
@@ -29,21 +35,29 @@ public class Moto {
     @Column(name = "cilindraje")
     private Double cilindraje;
 
+    @NotNull(message = "El tipo de reparación es obligatorio")
+    @Column(name = "tipoReparacion")
+    private String tipoReparacion;
+
     public Moto() {}
 
+    
     public int getIdMoto() { return idMoto; }
     public void setIdMoto(int idMoto) { this.idMoto = idMoto; }
 
-    public int getIdUsuario() { return idUsuario; }
-    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
+    public Usuarios getUsuario() { return usuario; }
+    public void setUsuario(Usuarios usuario) { this.usuario = usuario; }
     
-    public int getIdMarca() { return idMarca; }
-    public void setIdMarca(int idMarca) { this.idMarca = idMarca; }
+    public Marca getMarca() { return marca; }
+    public void setMarca(Marca marca) { this.marca = marca; }
     
     public String getModelo() { return modelo; }
     public void setModelo(String modelo) { this.modelo = modelo; }
     
     public Double getCilindraje() { return cilindraje; }
     public void setCilindraje(Double cilindraje) { this.cilindraje = cilindraje; }
+
+    public String getTipoReparacion() { return tipoReparacion; }
+    public void setTipoReparacion(String tipoReparacion) { this.tipoReparacion = tipoReparacion; }
 
 }
