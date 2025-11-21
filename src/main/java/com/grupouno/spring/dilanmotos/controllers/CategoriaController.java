f5b68c1a27100a2d46ba319cce8b0ae93bde97d8
Diver Mauricio Osma Manrique <diver1402@gmail.com>
2025-11-21 11:03:54 -0500

    Correciones en la vista categoria

--- src/main/java/com/grupouno/spring/dilanmotos/controllers/CategoriaController.java
+++ src/main/java/com/grupouno/spring/dilanmotos/controllers/CategoriaController.java
@@ -6,6 +6,7 @@ import com.grupouno.spring.dilanmotos.repositories.CategoriaRepository;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,22 +31,22 @@ public class CategoriaController {
         return "categoria";
     }
 
-    @PostMapping("/crear")
-    public String crearCategoria(
-            @Valid @ModelAttribute("nuevaCategoria") Categoria categoria,
-            BindingResult result,
-            Model model) {
-
+     @PostMapping("/categoria")
+    public String guardarCategoria(
+        @Valid @NonNull @ModelAttribute("nuevaCategoria") Categoria categoria,
+        BindingResult result,
+        Model model
+    ) {
         if (result.hasErrors()) {
             model.addAttribute("categorias", categoriaRepository.findAll());
             return "categoria";
         }
 
         categoriaRepository.save(categoria);
-        return "redirect:/categoria";
+        return "redirect:/categoria?creado";
     }
 
-     @GetMapping("/editar/{id}")
+     @GetMapping("/categoria/editar/{id}")
     public String editarCategoria(@PathVariable int id, Model model) {
         Categoria categoria = categoriaRepository.findById(id).orElse(null);
 
@@ -56,5 +57,31 @@ public class CategoriaController {
         model.addAttribute("categoria", categoria);
         return "categoria-editar";
     }
+
+
+    //Actualizar las categorías
+    @PostMapping("/categoria/actualizar")
+    public String actualizarCategoria(
+        @Valid @NonNull @ModelAttribute("categoriaEditada") Categoria categoria,
+        BindingResult result
+    ) {
+        if (result.hasErrors()) {
+            return "editar_categoria";
+        }
+
+        categoriaRepository.save(categoria);
+        return "redirect:/categoria?actualizado";
+    }
+
+    // Eliminar categoría
+    @GetMapping("/categoria/eliminar/{id}")
+    public String eliminarCategoria(@PathVariable("id") int id) {
+        if (categoriaRepository.existsById(id)) {
+            categoriaRepository.deleteById(id);
+            return "redirect:/categoria?eliminado";
+        }
+        return "redirect:/categoria?error=not_found";
+    }
+
 
 }
